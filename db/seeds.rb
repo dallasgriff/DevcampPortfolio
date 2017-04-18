831ab70989300e3702bb70a40c916428f70a0f1a
Dallas Griffiths <dallasgriff@gmail.com>
2017-04-18 14:59:24 -0600

    Integrated technologies model

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,7 @@ end
 puts "3 Topics created"
 
 10.times do |blog|
- Blog.create!( title: " my blog#{blog}", body: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet srisus. Nullam id dolor id nibh ultricies vehicula ut id elit.", topic_id: Topic.last.id
+ 	Blog.create!( title: " my blog#{blog}", body: "Aenean eu leo quam. Pellentesque ornare sem lacinia quam venenatis vestibulum. Aenean lacinia bibendum nulla sed consectetur. Curabitur blandit tempus porttitor. Praesent commodo cursus magna, vel scelerisque nisl consectetur et. Nullam quis risus eget urna mollis ornare vel eu leo. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet srisus. Nullam id dolor id nibh ultricies vehicula ut id elit.", topic_id: Topic.last.id
  	)
 end
 
@@ -36,7 +36,20 @@ end
 		body: "Curabitur blandit tempus porttitor. Integer posuere erat a ante venenatis dapibus posuere velit aliquet. Sed posuere consectetur est at lobortis. Donec id elit non mi porta gravida at eget metus.",
 		main_image: "http://placehold.it/400x600" ,
 		thumb_image: "http://placehold.it/350x200"
-		)
+	)
 end
 
 puts "created 9 portfolio items"
+
+3.times do |technology|
+	Technology.create!(name: "Technology #{technology}", portfolio_id: Portfolio.last.id
+	)
+end
+
+puts "3 technologies created"
+
+
+
+
+
+
