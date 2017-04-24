0484b1b5aa2ac75eb9a68010a987a4e2ae43721e
Dallas Griffiths <dallasgriff@gmail.com>
2017-04-24 15:09:25 -0600

    fixed pages and added partials

--- app/controllers/concerns/current_user_concern.rb
+++ app/controllers/concerns/current_user_concern.rb
@@ -7,9 +7,9 @@ module CurrentUserConcern
 
   def guest_user
   	OpenStruct.new(name: "Guest User", 
-  					first_name: "Guest", 
-  					last_name: "User", 
-  					email: "guest@example.com"
-  					)
+  					       first_name: "Guest", 
+  					       last_name: "User", 
+  					       email: "guest@example.com"
+  					       )
   end
 end
