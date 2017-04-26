7da28ba0e003846bd81bfe2237765592bdae2a75
Dallas Griffiths <dallasgriff@gmail.com>
2017-04-25 20:04:30 -0600

    fixed bug

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,6 +1,6 @@
 module ApplicationHelper
   def login_helper style = ''
-    if current_user.is_a?(GuestUser)
+    if current_user.is_a?(User)
       (link_to "Register", new_user_registration_path, class: style) +
       " ".html_safe +
       (link_to "Login", new_user_session_path, class: style)
