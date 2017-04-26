dc1db680ab1edd567ba32aeab7b0442648f00064
Dallas Griffiths <dallasgriff@gmail.com>
2017-04-26 14:16:27 -0600

    still trying to debug

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,4 +3,18 @@ class ApplicationController < ActionController::Base
   include DeviseWhitelist
   include SetSource
   include CurrentUserConcern
+
+  before_action :set_copyright
+
+  def set_copyright
+  	@copyright = DallasViewTool::Renderer.copyright 'Dallas Griffiths' | 'All rights reserved'
+  end	
+end
+
+module DallasViewTool
+	class Renderer
+		def self.copyright name, msg
+			"&copy; #{Time.now.year} | <b>#{name}</b> #{msg}".html_safe
+		end	
+	end
 end
