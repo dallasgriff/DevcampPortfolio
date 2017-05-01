aeee98fbbc924326f833fe7187b407c635c03a9a
Dallas Griffiths <dallasgriff@gmail.com>
2017-04-30 20:01:59 -0600

    adjusting page content to match previous work

--- app/controllers/concerns/default_page_content.rb
+++ app/controllers/concerns/default_page_content.rb
@@ -6,7 +6,7 @@ module DefaultPageContent
   end
 
   def set_page_defaults
-    @page_title = "Devcamp Portfolio | My Portfolio Website"
-    @seo_keywords = "Jordan Hudgens portfolio"
+    @page_title = "Dallas Griffiths | My Portfolio "
+    @seo_keywords = "Dallas Griffiths portfolio"
   end
 end
