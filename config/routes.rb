18593bbdb1c8bc9501c9a60e91b4f1ee520ab6be
Dallas Griffiths <dallasgriff@gmail.com>
2017-05-06 23:21:39 -0600

    added, removed, edited files for final product

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  resources :topics, only: [:index, :show]
+
   devise_for :users, path: '', path_names: { sign_in: 'login', sign_out: 'logout', sign_up: 'register' }
   resources :portfolios, except: [:show] do
     put :sort, on: :collection
@@ -16,5 +18,7 @@ Rails.application.routes.draw do
     end
   end
   
+  mount ActionCable.server => '/cable'
+
   root to: 'pages#home'
 end
