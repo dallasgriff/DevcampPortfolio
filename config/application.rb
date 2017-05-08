c58f42ac2263df68c65a9e9f57c693857ff3c592
Dallas Griffiths <dallasgriff@gmail.com>
2017-05-08 14:53:19 -0600

    updated secret key base

--- config/application.rb
+++ config/application.rb
@@ -20,5 +20,6 @@ Bundler.require(*Rails.groups)
 module DevcampPortfolio
   class Application < Rails::Application
   	config.eager_load_paths << "#{Rails.root}/Lib"
+  	config.secret_key_base = ENV["SECRET_KEY_BASE"]
   end
 end
